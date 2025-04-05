96cd2735580526d92c79e211476a63e095e568ba
Pablo Querales <pabloquerales20@gmail.com>
2025-04-05 16:35:41 +0200

    cambios

--- src/front/js/pages/ShoppingList.jsx
+++ src/front/js/pages/ShoppingList.jsx
@@ -3,10 +3,15 @@ import { useNavigate } from "react-router-dom";
 
 export default function ShoppingList() {
 	const navigate = useNavigate();
-
+	const handleClick = () => {
+		console.log("esto es una prueba");
+	};
 	return (
 		<div>
 			<h1>Esto va ser una lista de compras</h1>
+			<button type="button" className="btn-item btn btn-secondary btn-modal" onClick={handleClick}>
+				<i class="bi bi-house"></i>
+			</button>
 			<button
 				type="button"
 				className="btn-item home btn btn-secondary btn-modal"
